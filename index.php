75ff265131db68f3f5bb121266e171301db1619e
Ben Burwell <bburwell1@gmail.com>
2013-05-22 12:27:43 -0400

    Adds index file

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+	<title>Ben Burwell’s Playground</title>
+	<style type="text/css">
+		html {
+			background-color: #eee;
+			color: #333;
+			font-family: 'Helvetica Neue', 'Helvetica', sans-serif;
+			text-align: center;
+			font-weight: 200;
+		}
+
+		ul {
+			list-style-type: none;
+		}
+
+		a {
+			color: #900;
+			text-decoration: none;
+		}
+
+		a:hover {
+			background-color: #fff;
+		}
+
+		body {
+			padding: 100px;
+		}
+
+		li {
+			font-size: 18px;
+		}
+	</style>
+</head>
+<body>
+	<h1>Ben’s Playground</h1>
+	<p>Links to projects are below, or you can <a href="https://github.com/bburwell/Playground/">browse the source code on GitHub</a>.</p>
+	
+	<ul>
+	
+	<?php
+
+	// files to not print
+	$ignore_files = array('.', '..');
+
+	// get directory contents
+	$files = scandir(getcwd());
+
+	// loop in all files
+	foreach ($files as $file) {
+
+		// print directories not to be ignored
+		if (is_dir($file) && !in_array($file, $ignore_files)) {
+			echo '<li>';
+			echo '<a href="/'.$file.'/">'.$file.'</a>';
+			echo '</li>';
+		}
+
+	}
+
+	?>
+
+	</ul>
+</body>
+</html>
